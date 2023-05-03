c4706da3aaa0d0be3bb53f61013e189e5dbf72aa
Castillo Rios Sergio Alejandro <113485021+SergioCastill@users.noreply.github.com>
2023-05-03 14:52:15 -0600

    Código completo xD

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Communications, { text } from "react-native-communications";
+import { Ionicons } from "@expo/vector-icons";
+import { TextInput } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+
+
+
 
 export default function App() {
+  const [texto, setTexto] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Encabezado}>App Comunicaciones</Text>
+            <TextInput
+            backgroundColor="lightblue"
+        editable
+        maxLength={40}
+        onChangeText={text => setTexto(text)}
+        value={texto}
+        style={{padding: 10}}
+      />
+      <View style={styles.contenedorROW}>
+                        <TouchableOpacity
+          style={styles.touch}
+          onPress={() =>
+            Communications.text(
+            texto,
+              "Holiwis amigo"
+            )
+          }
+        >
+           <Ionicons name="chatbox-ellipses-outline" size={30} color="red" />
+        </TouchableOpacity>
+               
+          <TouchableOpacity
+          style={styles.touch}
+          onPress={() =>
+            Communications.email(
+              [texto],
+              null,
+              null,
+              "Ejemplo Email",
+              "Contenido de ejemplo prueba de email"
+            )
+          }
+        >
+          <Ionicons name="mail" size={30} color="red" />
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +62,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Encabezado:{
+    textAlign: "center",
+    fontSize: 25,
+    color: "blue",
+    fontWeight: "bold",
+  },
+    botones: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 20,
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+    touch: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  TextInput:{
+    flexDirection:"row",
+    textAlign:"center",
+    height: 20,
+  },
+  contenedorROW:{
+    flexDirection:"row",
+    alignItems:"center",
+  },
+  
 });
